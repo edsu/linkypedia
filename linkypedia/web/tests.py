25c67805e1f9105ca66150aa7d9f6fa6535037d6
Ed Summers <ehs@pobox.com>
2010-07-28 11:49:07 +0000

    added a simple test

--- linkypedia/web/tests.py
+++ linkypedia/web/tests.py
@@ -1,23 +1,10 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from linkypedia import wikipedia
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class WikipediaTest(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_info(self):
+        info = wikipedia.info('Pierre-Charles_Le_Sueur')
+        self.assertEqual(info['pageid'], 842462)
+        self.assertEqual(info['title'], 'Pierre-Charles Le Sueur')
