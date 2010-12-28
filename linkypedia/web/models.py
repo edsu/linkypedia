359abc742a7d5f9c703472002a31e5af76cad820
Ed Summers <ehs@pobox.com>
2010-12-27 23:30:38 -0500

    cleared out old views/templates to make way for new ones

--- linkypedia/web/models.py
+++ linkypedia/web/models.py
@@ -10,60 +10,6 @@ from linkypedia.rfc3339 import rfc3339_parse
 from linkypedia.wikipedia import api
 
 
-class WikipediaCategory(m.Model):
-    title = m.CharField(primary_key=True, max_length=255)
-
-    def __unicode__(self):
-        return self.title
-
-
-class WikipediaPage(m.Model):
-    url = m.CharField(null=False, max_length=500)
-    last_modified = m.DateTimeField(null=False)
-    title = m.TextField()
-    categories = m.ManyToManyField(WikipediaCategory, related_name='pages')
-
-    @classmethod
-    def new_from_wikipedia(klass, url):
-        # if we have a page for a given url already we can return it
-        wikipedia_pages = WikipediaPage.objects.filter(url=url)
-        if wikipedia_pages.count() > 0:
-            return wikipedia_pages[0], False
-
-        title_escaped = api.url_to_title(url)
-        info = api.info(title_escaped)
-
-        wikipedia_page = WikipediaPage.objects.create(title=info['title'],
-            url=url, last_modified=rfc3339_parse(info['touched']))
-
-        for cat in api.categories(wikipedia_page.title):
-            title = cat['title'][9:]
-            if not title:
-                continue
-            category, created = WikipediaCategory.objects.get_or_create(title=title)
-            wikipedia_page.categories.add(category)
-        wikipedia_page.save()
-        return wikipedia_page, True
-
-    def associated_username(self):
-        """
-        If the page is associated with a users profile it returns
-        that username.
-        """
-        match = re.search(r'^User.*?:([^/]+)', self.title)
-        if match:
-            return match.group(1)
-        else:
-            return None
-
-
-class Link(m.Model):
-    created = m.DateTimeField(auto_now_add=True)
-    wikipedia_page = m.ForeignKey('WikipediaPage', related_name='links')
-    target = m.TextField()
-    website = m.ForeignKey('Website', related_name='links')
-
-
 class Website(m.Model):
     url = m.TextField()
     name = m.TextField()
@@ -87,31 +33,8 @@ class Website(m.Model):
             return self.crawls.all()[0]
         return None
 
-    def categories(self):
-        return WikipediaCategory.objects.filter(pages__links__website=self).distinct().annotate(Count('pages'))
-
-    # TODO: is this still used?
-    def wikipedia_pages(self):
-        return WikipediaPage.objects.filter(links__website=self).distinct()
-
     def __unicode__(self):
         return "%s <%s> (%s)" % (self.name, self.url, self.id)
-
-
-class WikipediaUser(m.Model):
-    username = m.CharField(primary_key=True, max_length=255)
-    registration = m.DateTimeField(max_length=255, null=True)
-    wikipedia_pages = m.ManyToManyField('WikipediaPage', related_name='users')
-    gender = m.TextField(null=True)
-    edit_count = m.IntegerField(default=0)
-    created = m.DateTimeField(auto_now_add=True)
-    emailable = m.BooleanField(default=False)
-
-
-class WikipediaGroup(m.Model):
-    name = m.TextField()
-    wikipedia_users = m.ManyToManyField('WikipediaUser', related_name='groups')
-    created = m.DateTimeField(auto_now_add=True)
 
 
 class Crawl(m.Model):
--- linkypedia/web/views.py
+++ linkypedia/web/views.py
@@ -1,10 +1,5 @@
 import json
-import urllib2
-import datetime
-import urlparse
-import cStringIO
 
-from lxml import etree
 from django.db.models import Count
 from django.template import RequestContext
 from django.core.paginator import Paginator
@@ -17,146 +12,23 @@ from linkypedia.web import models as m
 from linkypedia.paginator import DiggPaginator
 from linkypedia.settings import CRAWL_CUTOFF, CACHE_TTL_SECS
 
-def about(request):
-    return render_to_response('about.html')
 
 @cache_page(CACHE_TTL_SECS)
 def home(request):
     return render_to_response('home.html', dictionary=locals(),
             context_instance=RequestContext(request))
 
-@cache_page(CACHE_TTL_SECS)
-def websites(request):
-    websites = m.Website.objects.all()
-    websites = websites.annotate(Count('links'))
-    websites = websites.order_by('-links__count')
-    host = request.get_host()
-
-    return render_to_response('websites.html', dictionary=locals(),
-            context_instance=RequestContext(request))
-
-def websites_feed(request):
-    websites = m.Website.objects.all()
-    websites = websites.order_by('-created')
-    host = request.get_host()
-
-    # figure out the last time the feed changed based on the
-    # most recently crawled site
-    feed_updated = datetime.datetime.now()
-    if websites.count() > 0:
-        feed_updated = websites[0].created
-
-    return render_to_response('websites.atom', dictionary=locals(),
-            context_instance=RequestContext(request),
-            mimetype='application/json; charset=utf-8')
-
-def website_summary(request, website_id):
-    website = get_object_or_404(m.Website, id=website_id)
-    tab = 'summary'
-    tab_summary = "Summary Information for %s" % website.name
-    title = "website: %s" % website.url
-    if website.links.count() == CRAWL_CUTOFF:
-        cutoff = CRAWL_CUTOFF
-    return render_to_response('website_summary.html', dictionary=locals())
-
-def website_pages(request, website_id):
-    website = get_object_or_404(m.Website, id=website_id)
-
-    page_num = request.GET.get('page', 1)
-    page_num = int(page_num)
-
-    # make sure we support the order
-    order = request.GET.get('order', 'update')
-    direction = request.GET.get('direction', 'desc')
-    other_direction = 'asc' if direction == 'desc' else 'desc'
-
-    if order == 'update' and direction =='asc':
-        sort_order = 'last_modified'
-    elif order == 'update' and direction == 'desc':
-        sort_order = '-last_modified'
-    elif order == 'links' and direction == 'asc':
-        sort_order = 'links__count'
-    else:
-        sort_order = '-links__count'
-
-    wikipedia_pages = m.WikipediaPage.objects.filter(links__website=website)
-    wikipedia_pages = wikipedia_pages.annotate(Count('links'))
-    wikipedia_pages = wikipedia_pages.order_by(sort_order)
-    wikipedia_pages = wikipedia_pages.distinct()
-
-    paginator = DiggPaginator(wikipedia_pages, 100)
-    page = paginator.page(page_num)
-    wikipedia_pages = page.object_list
-
-    tab = 'pages'
-    tab_summary = "wikipedia pages %s" % website.name 
-    title = "website: %s" % website.url
-
-    return render_to_response('website_pages.html', dictionary=locals())
-
-
-def website_page_links(request, website_id, page_id):
-    website = get_object_or_404(m.Website, id=website_id)
-    wikipedia_page = m.WikipediaPage.objects.get(id=page_id)
-    links = m.Link.objects.filter(wikipedia_page=wikipedia_page,
-            website=website)
-
-    return render_to_response('website_page_links.html', dictionary=locals())
-
-def website_pages_feed(request, website_id, page_num=1):
-    website = get_object_or_404(m.Website, id=website_id)
-    wikipedia_pages = m.WikipediaPage.objects.filter(links__website=website)
-    wikipedia_pages = wikipedia_pages.annotate(Count('links'))
-    wikipedia_pages = wikipedia_pages.order_by('-last_modified')
-    wikipedia_pages = wikipedia_pages.distinct()
-
-    feed_updated = datetime.datetime.now()
-    if wikipedia_pages.count() > 0:
-        feed_updated = wikipedia_pages[0].last_modified
-
-    host = request.get_host()
-    paginator = Paginator(wikipedia_pages, 100)
-    page = paginator.page(int(page_num))
-    wikipedia_pages = page.object_list
-    
-    return render_to_response('website_pages_feed.atom', 
-            mimetype="application/atom+xml", dictionary=locals())
-
-def website_categories(request, website_id, page_num=1):
-    website = get_object_or_404(m.Website, id=website_id)
-    categories = website.categories().order_by('-pages__count')
-    paginator = DiggPaginator(categories, 100)
-    page = paginator.page(int(page_num))
-    categories = page.object_list
-    tab = 'categories'
-    tab_summary = "Categories for %s" % website.name 
-    title = "website: %s" % website.url
-    return render_to_response('website_categories.html', dictionary=locals())
-
-def website_users(request, website_id):
-    website = get_object_or_404(m.Website, id=website_id)
-    users = m.WikipediaUser.objects.filter(wikipedia_pages__links__website=website)
-    users = users.distinct()
-    users = users.order_by('username')
-    tab = 'users'
-    title = "website: %s" % website.url
-    return render_to_response('website_users.html', dictionary=locals())
-
 def lookup(request):
     url = request.REQUEST.get('url', None)
     results = []
-    for link in m.Link.objects.filter(target=url):
-        w = link.wikipedia_page
+    for link in m.ExternalLink.objects.filter(url=url):
+        w = link.article
         result = {
             'url': w.url, 
             'title': w.title, 
-            'last_modified': rfc3339(w.last_modified)
             }
         results.append(result)
     return HttpResponse(json.dumps(results, indent=2), mimetype='application/json')
-
-def robots(request):
-    return render_to_response('robots.txt', mimetype='text/plain')
 
 def status(request):
     link = m.ExternalLink.objects.all().order_by('-id')[0]
@@ -170,3 +42,9 @@ def status(request):
 
     return HttpResponse(json.dumps(update, indent=2), 
                         mimetype='application/json')
+
+def robots(request):
+    return render_to_response('robots.txt', mimetype='text/plain')
+
+def about(request):
+    return render_to_response('about.html')
